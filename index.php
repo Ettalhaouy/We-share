e6df03472549f9399ec2b224d46d4963292842f3
ismailchakrane <ism.chakrane@gmail.com>
2021-04-09 09:57:08 +0100

    changing index

--- index.php
+++ index.php
@@ -27,15 +27,15 @@ $nb_ads = (int) $nb_advertisements->nb;
   <!-- Page Content -->
   <div class="container" style="margin-top: 3%;">
     <div class="row justify-content-around">
-
       <?php
-
       for ($i = 0; $i < $nb_ads; $i++) {
 
         $id = (int) $advertisements[$i]->id;
         $current_ads = $db->query('SELECT title,nb_Donation,id_organisaton,photo,date,SUBSTRING(Description, 1, 160) as text FROM `advertisements` WHERE id = ?; ', [$id])->fetch();
         $orgId = (int) $current_ads->id_organisaton;
         $org = $db->query('SELECT * FROM organisations WHERE id = ?; ', [$current_ads->id_organisaton])->fetch();
+        $randNumModel = rand(0,100);
+                
         if (empty(Session::getInstance()->read('id'))) {
           echo '
     <div class="col-lg-4 mb-3 shadow p-3 mb-5 bg-white rounded align-items-center">
@@ -65,7 +65,7 @@ $nb_ads = (int) $nb_advertisements->nb;
             </svg>
             Faire un don
         </button>
-        <button type="button" data-toggle="modal" data-target="#myModal' . $orgId . '" class="btn btn-secondary btn-block">
+        <button type="button" data-toggle="modal" data-target="#myModal' . $randNumModel . '" class="btn btn-secondary btn-block">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -85,13 +85,13 @@ $nb_ads = (int) $nb_advertisements->nb;
               fetch("extrainfos.json")
               .then(response => response.json())
               .then(data => {
-                id = ' . $orgId . ';
+                id = ' . $org->id . ';
                 console.log(id);
                 for(i=0;i<Object.keys(data[id]).length ;i++){
                   const header =  `<div class="header">${Object.keys(data[id])[i]}</div>`;
                   const content = `<div class="content">${Object.values(data[id])[i]}</div>`;
-                  document.getElementById(\'modal-elt' . $orgId . '\').innerHTML += header;
-                  document.getElementById(\'modal-elt' . $orgId . '\').innerHTML += content;
+                  document.getElementById(\'modal-elt' . $randNumModel . '\').innerHTML += header;
+                  document.getElementById(\'modal-elt' . $randNumModel . '\').innerHTML += content;
                 }
               })
               </script>
@@ -107,41 +107,42 @@ $nb_ads = (int) $nb_advertisements->nb;
               margin-bottom: 10%;
             }
           </style>
-        <!-- The Modal -->
-        <div class="modal fade" id="myModal' . $orgId . '">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-          <div class="modal-content">
-
-            <!-- Modal Header -->
-            <div class="modal-header">
-              <h4 class="modal-title">À propos de ' . $org->login . '</h4>
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <!-- The Modal -->
+          <div class="modal fade" id="myModal' . $randNumModel . '">
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+    
+              <!-- Modal Header -->
+              <div class="modal-header">
+                <h4 class="modal-title">À propos de ' . $org->login . '</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+              </div>
+    
+              <!-- Modal body -->
+              <div class="modal-body">
+                <center>
+                <img style="width: 40%; height: 10%; border-radius: 50%;" class="mb-4 rounded-circle" src="assets/images/We-Share-logo.png" alt="">
+                <br>
+                <div id="modal-elt' . $randNumModel . '"></div>
+    
+                <p class="mt-5 mb-3 text-muted">&copy; We Share 2021</p>
+                </center>
+              </div>
+    
+              <!-- Modal footer -->
+              <div class="modal-footer">
+    
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              </div>
+    
             </div>
-
-            <!-- Modal body -->
-            <div class="modal-body">
-              <center>
-              <img style="width: 40%; height: 10%; border-radius: 50%;" class="mb-4 rounded-circle" src="assets/images/We-Share-logo.png" alt="">
-              <br>
-              <div id="modal-elt' . $orgId . '"></div>
-
-              <p class="mt-5 mb-3 text-muted">&copy; We Share 2021</p>
-              </center>
-            </div>
-
-            <!-- Modal footer -->
-            <div class="modal-footer">
-
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-
           </div>
-        </div>
-      </div>
+        </div>  
       <form>
     </div>
   </div>
       ';
+ 
         } else {
           echo '
       <div class="col-lg-4 mb-3 shadow p-3 mb-5 bg-white rounded align-items-center">
@@ -170,7 +171,7 @@ $nb_ads = (int) $nb_advertisements->nb;
               </svg>
               Consulter
           </button>
-          <button type="button"  data-toggle="modal" data-target="#myModal' . $orgId . '"  class="btn btn-secondary btn-block">
+          <button type="button"  data-toggle="modal" data-target="#myModal' . $randNumModel. '"  class="btn btn-secondary btn-block">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="25" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                   <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -190,13 +191,13 @@ $nb_ads = (int) $nb_advertisements->nb;
                   fetch("extrainfos.json")
                   .then(response => response.json())
                   .then(data => {
-                    id = ' . $orgId . ';
+                    id = ' . $org->id . ';
                     console.log(id);
                     for(i=0;i<Object.keys(data[id]).length ;i++){
                       const header =  `<div class="header">${Object.keys(data[id])[i]}</div>`;
                       const content = `<div class="content">${Object.values(data[id])[i]}</div>`;
-                      document.getElementById(\'modal-elt' . $orgId . '\').innerHTML += header;
-                      document.getElementById(\'modal-elt' . $orgId . '\').innerHTML += content;
+                      document.getElementById(\'modal-elt' . $randNumModel  . '\').innerHTML += header;
+                      document.getElementById(\'modal-elt' . $randNumModel  . '\').innerHTML += content;
                     }
                   })
               </script>
@@ -213,7 +214,7 @@ $nb_ads = (int) $nb_advertisements->nb;
                 }
               </style>
             <!-- The Modal -->
-            <div class="modal fade" id="myModal' . $orgId . '">
+            <div class="modal fade" id="myModal' . $randNumModel  . '">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
               <div class="modal-content">
 
@@ -228,7 +229,7 @@ $nb_ads = (int) $nb_advertisements->nb;
                   <center>
                   <img style="width: 40%; height: 10%; border-radius: 50%;" class="mb-4 rounded-circle" src="assets/images/We-Share-logo.png" alt="">
                   <br>
-                  <div id="modal-elt' . $orgId . '"></div>
+                  <div id="modal-elt' . $randNumModel  . '"></div>
 
                   <p class="mt-5 mb-3 text-muted">&copy; We Share 2021</p>
                   </center>
@@ -251,7 +252,6 @@ $nb_ads = (int) $nb_advertisements->nb;
         }
       }
       ?>
-
     </div>
   </div>
 
